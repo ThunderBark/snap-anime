24175ff5aa1043afb6def9c713d3996e98c8ab14
Ivan Enusov <ivan.enusov@gmail.com>
2022-05-31 23:51:24 +0700

    Working file input

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import React, { DragEvent, useState } from "react";
+import React, { ChangeEvent, DragEvent, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import "./App.css";
+
+/* TODO: API supports additional types: video/*, application/octet-stream and application/x-www-form-urlencoded
+  need to do something with them*/
 
 function App() {
   const [image, setImage] = useState("");
@@ -13,8 +16,6 @@ function App() {
       if (ev.dataTransfer.items.length > 1) {
         toast.error("Can't handle multiple files");
       } else {
-        /* TODO: API supports additional types: video/*, application/octet-stream and application/x-www-form-urlencoded
-        need to do something with them*/
         if (ev.dataTransfer.items[0].type.includes("image/")) {
           const file = ev.dataTransfer.items[0].getAsFile();
           file && setImage(URL.createObjectURL(file));
@@ -22,6 +23,13 @@ function App() {
           toast.error("Dropped file is not an image");
         }
       }
+    }
+  };
+
+  const fileChangeHandler = (ev: ChangeEvent<HTMLInputElement>) => {
+    const files = ev.currentTarget.files;
+    if (files && files.length > 0) {
+      setImage(URL.createObjectURL(files[0]));
     }
   };
 
@@ -34,13 +42,28 @@ function App() {
     <div className="App">
       <Toaster position="top-right" reverseOrder={false} />
       <img src={image} alt=""></img>
-      <div
+      <br />
+      <label
         className="dropzone"
         onDrop={dropHandler}
         onDragOver={dragOverHandler}
+        htmlFor="fileInput"
       >
-        <p>Drop frame from anime</p>
-      </div>
+        Drop frame from anime here
+      </label>
+      <input
+        type="file"
+        accept="image/*"
+        id="fileInput"
+        style={{ display: "none" }}
+        onChange={fileChangeHandler}
+      />
+      <div>or</div>
+      <input placeholder="Enter image URL"></input>
+      <button>Submit</button>
+      <br />
+      <div>then</div>
+      <button>Snap!</button>
     </div>
   );
 }
